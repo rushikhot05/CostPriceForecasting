4b4f67a8fe6918b1cfdbf11209be20a4de246b66
Rushikesh Khot <rushi.khot05@gmail.com>
2019-06-18 14:38:59 +0530

    Add files via upload

---
+++ ARIMA.py
@@ -0,0 +1,38 @@
+from pandas import Series
+from statsmodels.tsa.arima_model import ARIMA
+import numpy
+
+# create a differenced series
+def difference(dataset, interval=1):
+	diff = list()
+	for i in range(interval, len(dataset)):
+		value = dataset[i] - dataset[i - interval]
+		diff.append(value)
+	return numpy.array(diff)
+
+# invert differenced value
+def inverse_difference(history, yhat, interval=1):
+	return yhat + history[-interval]
+
+# load dataset
+series = Series.from_csv('dataset.csv', header=None)
+# seasonal difference
+X = series.values
+days_in_year = 365
+differenced = difference(X, days_in_year)
+# fit model
+model = ARIMA(differenced, order=(7,0,1))
+model_fit = model.fit(disp=0)
+# multi-step out-of-sample forecast
+a = input("Enter no. of days.")
+start_index = len(differenced)
+end_index = start_index + int(a) -1
+forecast = model_fit.predict(start=start_index, end=end_index)
+# invert the differenced forecast to something usable
+history = [x for x in X]
+day = 1
+for yhat in forecast:
+	inverted = inverse_difference(history, yhat, days_in_year)
+	print('Day %d: %f' % (day, inverted))
+	history.append(inverted)
+	day += 1
